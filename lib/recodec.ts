2f44f8946655dca8fe032b07caed00c097b52354
Zhi.Huang <zh.impact@outlook.com>
2025-07-25 19:45:49 +0800

    chore: add return type to toUTF8 and reorder package.json metadata fields

--- lib/recodec.ts
+++ lib/recodec.ts
@@ -6,7 +6,7 @@ const errorlog = createDebug("toUTF8:error");
 const infolog = createDebug("toUTF8:info");
 const verbose = createDebug("toUTF8:verbose");
 
-export function toUTF8(input: ArrayBufferLike) {
+export function toUTF8(input: ArrayBufferLike): string {
   const buffer = Buffer.from(input);
 
   const detectedEncoding = chardet.detect(buffer);
